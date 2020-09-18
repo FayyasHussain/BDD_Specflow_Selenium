4b694ca3956ea32cb10eb494bb71d4cf613cd5d0
FayyasTH <fayashussain@gmail.com>
2020-09-18 12:40:40 +0530

    Added webdriver waits for finding elements

--- ComponentHelper/GenericHelper.cs
+++ ComponentHelper/GenericHelper.cs
@@ -7,6 +7,7 @@ using System.Collections.ObjectModel;
 using System.Drawing.Imaging;
 using System.Linq;
 using System.Net.NetworkInformation;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,7 +15,7 @@ namespace BDD_Specflow_Selenium.ComponentHelper
 {
     public class GenericHelper
     {
-       public static void TakeScreenshot(string fileName = "NoNameProvided")
+        public static void TakeScreenshot(string fileName = "NoNameProvided")
         {
             ITakesScreenshot screenShotDriver = ObjectRepository.Driver as ITakesScreenshot;
             Screenshot screenshot = screenShotDriver.GetScreenshot();
@@ -38,9 +39,80 @@ namespace BDD_Specflow_Selenium.ComponentHelper
 
         public static IWebElement FindElement(By by, int time)
         {
-            WebDriverWait wait = new WebDriverWait(ObjectRepository.Driver, TimeSpan.FromMilliseconds(time));
-            wait.IgnoreExceptionTypes(typeof(NotFoundException),typeof(TimeoutException),typeof(NoSuchElementException));
+            // Returns a IWebElement if found within 'time', else returns null
+            // Achieved through WebDriverExplicit waits.
+            // Add polling while instantiating webdriver wait.
+
+            WebDriverWait wait = Webdriverwait(time);
             return wait.Until(d => d.FindElement(by));
+        }
+
+        public static bool CheckElementExists(By by, int time)
+        {
+            try
+            {
+                IWebElement element = FindElement(by, time);
+                return (element != null) ? true : false;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static bool IsElementVisible(By by, int time)
+        {
+            WebDriverWait wait = Webdriverwait(time);
+            return wait.Until(function =>
+            {
+                try
+                {
+                    IWebElement element = ObjectRepository.Driver.FindElement(by);
+                    return element.Displayed;
+                }
+                catch (StaleElementReferenceException)
+                {
+                    return false;
+                }
+                catch (NoSuchElementException)
+                {
+                    return false;
+                }
+            });
+        }
+
+        private static WebDriverWait Webdriverwait(int time)
+        {
+            WebDriverWait wait = new WebDriverWait(ObjectRepository.Driver, TimeSpan.FromMilliseconds(time))
+            {
+                PollingInterval = TimeSpan.FromMilliseconds(500)
+            };
+            wait.IgnoreExceptionTypes(typeof(NoSuchElementException), typeof(WebDriverTimeoutException));
+            return wait;
+        }
+
+        public static bool RetryFindClick(By by, int time)
+        {
+            // Sometimes you need to click multiple times for the button to work.
+            bool elementClicked = false;
+            int numberOfTries = 5;
+            int attempt = 1;
+
+            while (attempt <= numberOfTries)
+            {
+                try
+                {
+                    FindElement(by, time).Click();
+                    elementClicked = true;
+                    break;
+                }
+                catch (StaleElementReferenceException)
+                {
+                    attempt++;
+                }
+            }
+
+            return elementClicked;
         }
     }
 }
